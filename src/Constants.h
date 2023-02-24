2c6345a4c10ca93dc6acce97922cd1e97642c6d3
furokl <furokl@mail.ru>
2023-02-24 13:40:38 +0300

    Add CMakeLists.txt

--- src/Constants.h
+++ src/Constants.h
@@ -3,7 +3,7 @@
 
 namespace cnst
 {
-    const char patch[] = "./file/ListRand.txt";
+    const char patch[] = "../file/ListRand.txt";
     const char separator = ' ';
     const int null_rand = -1;
 }
